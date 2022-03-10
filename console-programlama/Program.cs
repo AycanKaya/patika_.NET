8f7ac5f20457afba4c0accd5b95b3bc43367b0d0
Aycan <aycnkya12@gmail.com>
2022-03-10 17:35:37 +0300

    first

---
+++ console-programlama/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace console_programlama{
+    
+class Program{
+
+    public static void Main(string[] args){
+
+        Console.WriteLine("İsim ve Soyisim giriniz.");
+  //      string name= Console.ReadLine();
+   //     string surname=Console.ReadLine();
+     //   Console.WriteLine(name+" "+surname);
+
+
+        var date1 = new DateTime(2008, 5, 1, 8, 30, 52);
+        Console.WriteLine(date1);
+
+        int num=1;
+        int num2=2;
+        int a= num+num2;
+
+        string str20="20";
+        int x=20;
+        int y=x+int.Parse(str20);
+
+        bool isSucces=true;
+        bool isCompleted=false;
+
+        if(isSucces || isCompleted) {Console.WriteLine("Normall");}
+        if(isSucces && isCompleted) {Console.WriteLine("Great");}
+        if(!(isSucces || isCompleted)) {Console.WriteLine("Bad");}
+
+        //***********Implicit Conversion
+
+        byte b=9;
+        sbyte c=8;
+        short s=4;
+        int d=b+c+s;
+        Console.WriteLine("d: "+d);
+
+        long h=d;
+        Console.WriteLine("h:"+h);
+        char f = 'k';
+        object o=f+h;
+        Console.WriteLine("o:"+o);
+
+        //**********Explicit Conversion
+
+        int ı=4;
+        byte yb=((byte)ı);
+
+        string ss=ı.ToString();
+        string zz=14.5f.ToString();
+
+
+    
+
+    }
+    /*
+        Task
+        Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), 
+        and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. 
+        Round the result to the nearest integer.
+    */
+    public static void solve(double meal_cost, int tip_percent, int tax_percent){
+
+
+    double tip=tip_percent*meal_cost/100;
+    double tax=tax_percent*meal_cost/100;
+    double total=meal_cost+tip+tax;
+    Console.WriteLine(Math.Round(total));
+    
+    
+
+}
+
+}
+    
+
+}
---
+++ hata_yönetimi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace hata_yönetimi
+{
+    class Program{
+        public static void Main(string[] args){
+        
+            try{
+                int a = int.Parse(Console.ReadLine());
+
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a+b;
+
+                Console.WriteLine(c);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+            }
+            
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+
+
+
+
+            try
+            {
+                int a=int.Parse(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                
+                Console.WriteLine("Boş değer atayamazsınız.");
+                Console.WriteLine(ex);
+            }catch(FormatException ex){
+
+                Console.WriteLine("Uyumsuz veri tipi.");
+                Console.WriteLine(ex);
+
+            }catch(OverflowException o){
+                Console.WriteLine("Çok büyük ya da çok küçük bir değer girdiniz.");
+                Console.WriteLine(o);
+            }
+
+
+
+
+
+        }
+    }
+}
