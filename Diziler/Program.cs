b7aebdebe179dea5054a6c4b181a9055e2424067
Aycan <aycnkya12@gmail.com>
2022-03-10 21:11:49 +0300

    arrays

---
+++ Diziler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace diziler {
+    class Program{
+        public static void Main(string[] args){
+            string[] colors=new String[5];
+            string[] animals={"Dog","Cat","Snake", "Bird"};
+
+            Console.WriteLine("Please enter the count of element");
+            int lengthDizi=int.Parse(Console.ReadLine());
+            int[] numberArr=new int[lengthDizi];
+            int sum=0;
+            foreach(var n in numberArr)
+                sum+=n;
+            Console.WriteLine("Average : "+sum/numberArr);
+                
+
+
+
+        }
+    }
+}
