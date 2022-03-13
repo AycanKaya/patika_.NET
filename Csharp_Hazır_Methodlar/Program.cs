f4f617611bd53d2a4264e7950cb0a74c35c2fe2c
Aycan <aycnkya12@gmail.com>
2022-03-13 13:34:52 +0300

    Csharp Hazır Methodlar

---
+++ Csharp_Hazır_Methodlar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+      int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; ++i)
+        {
+            string sentence = Console.ReadLine();
+            string even = "";
+            string odd = "";
+         
+            for (int j = 0; j < sentence.Length - 1; j += 2)
+            {
+                even += sentence[j];
+                odd += sentence[j + 1];
+            }
+
+         
+            if (sentence.Length % 2 != 0)
+            {
+                even += sentence[sentence.Length - 1];    
+            }
+            
+            Console.WriteLine("{0} {1}", even, odd);
+        }
+    
+        
+    }
+}
